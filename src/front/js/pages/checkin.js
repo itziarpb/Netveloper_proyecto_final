85bc69e63b79e9f721c1e2a2227fec1a984a7b25
itziarpb <itziar.perezb@gmail.com>
2023-01-04 20:48:49 +0000

    checkin consegido

--- src/front/js/pages/checkin.js
+++ src/front/js/pages/checkin.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
+import { FormCheckin } from "../component/formCheckin";
+import "../../styles/checkin.css";
 
 import { Context } from "../store/appContext";
 
 export const Checkin = () => {
   const { store, actions } = useContext(Context);
 
-  return <div className="container"></div>;
+  return (
+    <div className="col-7 mx-auto ">
+      <h1 className="checkinTitle">Bienvenido a la página de registro</h1>
+      <FormCheckin />
+    </div>
+  );
 };
---
+++ src/front/js/pages/login.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export const Login = () => {
+  return <div className="container"></div>;
+};
