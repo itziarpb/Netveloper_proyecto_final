eaca13663f12f56624c19207c2fc578534dbbd64
chemawall <jmmuro88@gmail.com>
2023-01-11 20:01:38 +0000

    parametros id colocados, pero vuelta a empezar

--- src/front/js/component/jumbotron.js
+++ src/front/js/component/jumbotron.js
@@ -5,23 +5,23 @@ import { Link } from "react-router-dom";
 export const Jumbotron = () => {
   const { actions } = useContext(Context);
 
-  const [id1, setId1] = useState([]);
-  const [id2, setId2] = useState([]);
-  const [id3, setId3] = useState([]);
+  const [id1, setId1] = useState();
+  const [id2, setId2] = useState();
+  const [id3, setId3] = useState();
 
-  const [img, setImg] = useState([]);
-  const [img2, setImg2] = useState([]);
-  const [img3, setImg3] = useState([]);
+  const [img, setImg] = useState();
+  const [img2, setImg2] = useState();
+  const [img3, setImg3] = useState();
 
   /*---------------------------------------------------------------*/
   /* Llamadas a la api para obtener los ID de cada canal de youtube*/
   /*---------------------------------------------------------------*/
 
-  /*Canal de nategentile7*/
+  /*Canal de yacklyon5993*/
 
   useEffect(() => {
     fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=nategentile7&key=AIzaSyCLGNOAdPMC9vfdOzhZyOMMuO4OBOtvXZA`
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=yacklyon5993&key=AIzaSyCLGNOAdPMC9vfdOzhZyOMMuO4OBOtvXZA`
     )
       .then((response) => {
         console.log(response.ok); // will be true if the response is successfull
@@ -31,15 +31,16 @@ export const Jumbotron = () => {
 
       .then((response) => {
         setId1(response.items[0].id.channelId);
+       
       })
       .catch((error) => console.error("Error:", error));
   }, []);
 
-  /*Canal de midudev*/
+  /*Canal de fatztech*/
 
   useEffect(() => {
     fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=midudev&key=AIzaSyAEQjzh7hO7gU5VkQ0iahdTXNTIpLiaQ7Q`
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=FaztTech&key=AIzaSyAEQjzh7hO7gU5VkQ0iahdTXNTIpLiaQ7Q`
     )
       .then((response) => {
         console.log(response.ok); // will be true if the response is successfull
@@ -49,6 +50,8 @@ export const Jumbotron = () => {
 
       .then((response) => {
         setId2(response.items[0].id.channelId);
+        console.log(response.items[0].id.channelId);
+        
         
       })
       .catch((error) => console.error("Error:", error));
@@ -68,7 +71,7 @@ export const Jumbotron = () => {
 
       .then((response) => {
         setId3(response.items[0].id.channelId);
-        console.log(response.items[0].id.channelId);
+        
       })
       .catch((error) => console.error("Error:", error));
   }, []);
@@ -77,11 +80,11 @@ export const Jumbotron = () => {
   /*Llamadas a la api para obtener el channelbanner de cada canal*/
   /*---------------------------------------------------------------*/
 
-  /*ChannelBaner de nategentile7*/
+  /*ChannelBaner de yacklyon5993*/
 
   useEffect(() => {
     fetch(
-      `https://youtube.googleapis.com/youtube/v3/channels?part=brandingSettings&id=UC36xmz34q02JYaZYKrMwXng&key=AIzaSyCLGNOAdPMC9vfdOzhZyOMMuO4OBOtvXZA`
+      `https://youtube.googleapis.com/youtube/v3/channels?part=brandingSettings&id=${id1}&key=AIzaSyCLGNOAdPMC9vfdOzhZyOMMuO4OBOtvXZA`
     )
       .then((response) => {
         console.log(response.ok); // will be true if the response is successfull
@@ -94,13 +97,13 @@ export const Jumbotron = () => {
         console.log(response.items[0].brandingSettings.image.bannerExternalUrl);
       })
       .catch((error) => console.error("Error:", error));
-  }, []);
+  }, [id1]);
 
-  /*ChannelBaner de holamundo*/
+  /*ChannelBaner de Fazt*/
 
   useEffect(() => {
     fetch(
-      `https://youtube.googleapis.com/youtube/v3/channels?part=brandingSettings&id=UC4FHiPgS1KXkUMx3dxBUtPg&key=AIzaSyCLGNOAdPMC9vfdOzhZyOMMuO4OBOtvXZA`
+      `https://youtube.googleapis.com/youtube/v3/channels?part=brandingSettings&id=UCRWjpn9LNoQqhJ59AT_yxPw&key=AIzaSyCLGNOAdPMC9vfdOzhZyOMMuO4OBOtvXZA`
     )
       .then((response) => {
         console.log(response.ok); // will be true if the response is successfull
