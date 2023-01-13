1cf02a1f947279a1434e734b72e2a91bb2c16b03
chemawall <jmmuro88@gmail.com>
2023-01-13 14:31:12 +0000

    añadidas tablas nuevas para revisar

--- src/api/models.py
+++ src/api/models.py
@@ -3,6 +3,68 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+class Channel(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    channelId = db.Column(db.String(120), unique=True, nullable=False)
+    channelBanner = db.Column(db.String(120), unique=True, nullable=False)
+    channelTitle = db.Column(db.String(80), unique=True, nullable=False)
+    playList = 
+
+    def __repr__(self):
+        return f'<Channel {self.channelTitle}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "channelId": self.channelId,
+            "channelBanner": self.channelBanner,
+            "channelTitle": self.channelTitle
+
+        }
+
+class PlayListItems(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    playListId = db.Column(db.String(120), unique=True, nullable=False)
+    playListTitle = db.Column(db.String(80), unique=True, nullable=False)
+    playListDescription = db.Column(db.String(120), unique=True, nullable=False)
+    playListPosition = db.Column(db.Integer, unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<PlayListItems {self.playListTitle}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "playListId": self.playListId,
+            "playListTitle": self.playListTitle,
+            "playListDescription": self.playListDescription,
+            "playListPosition": self.playListPosition
+        }
+          
+
+class Video(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    videoId = db.Column(db.String(120), unique=True, nullable=False)
+    videoTitle = db.Column(db.String(80), unique=True, nullable=False)
+    videoDescription = db.Column(db.String(120), unique=True, nullable=False)
+    videoPlayer = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<Video {self.videoTitle}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "videoId": self.videoId,
+            "videoTitle": self.videoTitle,
+            "videoDescription": self.playvideoDescription,
+            "videoPlayer": self.videoPlayer
+        }
+
+
+    
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
