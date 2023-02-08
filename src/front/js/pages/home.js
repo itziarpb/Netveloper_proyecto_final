1cd9cd1bdcd88bca920c58b61ebe2baa7109338e
chemawall <jmmuro88@gmail.com>
2023-02-08 18:36:30 +0000

    ultimocomit

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,6 +7,7 @@ import { CarouselCss } from "../component/carouselCss";
 import { CarouselJs } from "../component/carouselJs";
 import "../../styles/home.css"
 import { ModalContact } from "../component/modalcontacto";
+import { Intro } from "../component/introduccion";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
@@ -15,6 +16,7 @@ export const Home = () => {
     <div className="container-fluid fondo">
       
       <Jumbotron/>
+      
       <div className="container-fluid bg-dark mt-5 pt-4 pb-4 banner1">
             <div className="d-flex justify-content-center"><p>¿Te gustaría subir tu contenido para ponerte a prueba?</p></div>
             <div className="d-flex justify-content-center"><button type="button" className="btn fondoboton d-flex justify-content-center"data-bs-toggle="modal" data-bs-target="#exampleModal">Contacta con nosotros</button></div>
