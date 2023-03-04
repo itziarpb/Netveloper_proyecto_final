2bd939bf7fcbeec9a0719f0b5cafbfe3cb00d094
itziarpb <itziar.perezb@gmail.com>
2023-03-04 13:06:05 +0000

    comentarios terminados

--- src/front/js/component/coments.js
+++ src/front/js/component/coments.js
@@ -6,8 +6,9 @@ import "../../styles/coments.css";
 export const Coments = (props) => {
   const { store, actions } = useContext(Context);
   const [coments, setComents] = useState();
-  const [comentData, setComentData] = useState();
+  const [comentData, setComentData] = useState(``);
   const [textValue, setTextValue] = useState();
+  const [inputValue, setInputValue] = useState("");
 
   const get_coment = () => {
     fetch(process.env.BACKEND_URL + "/api/coments/" + props.videoid)
@@ -27,10 +28,12 @@ export const Coments = (props) => {
 
   const handleChange = (event) => {
     setComentData({ ...comentData, [event.target.name]: event.target.value });
+    setInputValue(event.target.value)
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setInputValue("")
     fetch(process.env.BACKEND_URL + "/api/coment/" + props.videoid, {
       method: "POST",
       body: JSON.stringify(comentData),
@@ -44,7 +47,25 @@ export const Coments = (props) => {
       .then((response) => {
         console.log("Success:", response);
         get_coment();
-        // setTextValue("");
+      });
+  };
+  const handleDelete = (id) => {
+    fetch(process.env.BACKEND_URL + "/api/coment/" + id, {
+      method: "DELETE",
+      headers: {
+        // "Content-Type": "application/json",
+        Authorization: "Bearer " + store.token, //localStorage.getItem("token"),
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Hubo un error");
+        } else {
+          setComents((prev) => prev.filter((com) => com.id != id));
+        }
+      })
+      .catch((e) => {
+        console.log(e);
       });
   };
 
@@ -59,9 +80,14 @@ export const Coments = (props) => {
                   <div className="fw-bold">{item.user.username}:</div>
                   <div>{item.coment}</div>
                 </div>
-                <div className="col-1">
-                <div className="fa fa-trash"></div>
-                </div>
+                {store.dataUser.username == item.user.username && (
+                  <div className="col-1">
+                    <div
+                      className="fa fa-trash"
+                      onClick={() => handleDelete(item.id)}
+                    ></div>
+                  </div>
+                )}
               </div>
             );
           })}
@@ -75,7 +101,7 @@ export const Coments = (props) => {
               id=""
               placeholder="Comentario"
               onChange={handleChange}
-              value={textValue}
+              value={inputValue}
             ></textarea>
             <button className="btn" type="submit">
               Comentar
